a08cf8c08840e0f4313aff5d5c086dc816bea488
FTL Hyperspace <ftl-hyperspace@github.com>
2022-10-30 13:11:32 +0000

    Bump version to 1.2.11
    (cherry picked from commit 76d27330e47bdba823807c91b5fc2bf92893e26b)

--- HSVersion.h
+++ HSVersion.h
@@ -7,7 +7,7 @@ Change the version numbers here
 */
 #define HS_VER_MAJOR 1
 #define HS_VER_MINOR 2
-#define HS_VER_PATCH 9
+#define HS_VER_PATCH 11
 
 #define BUILD_IDENTIFIER_HASH "unknown_build"
 #define BUILD_BRANCH ""
